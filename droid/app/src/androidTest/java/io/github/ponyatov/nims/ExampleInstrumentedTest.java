d460777eaa19e6759686dc8095092e68aa61b52b
Dmitry Ponyatov <dponyatov@gmail.com>
2019-08-23 13:31:12 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- droid/app/src/androidTest/java/io/github/ponyatov/nims/ExampleInstrumentedTest.java
+++ droid/app/src/androidTest/java/io/github/ponyatov/nims/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package io.github.ponyatov.nims;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- droid/app/src/main/java/io/github/ponyatov/nims/MainActivity.java
+++ droid/app/src/main/java/io/github/ponyatov/nims/MainActivity.java
@@ -1,19 +1,25 @@
 package io.github.ponyatov.nims;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
     }
 
     /**
@@ -21,9 +27,4 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 }
