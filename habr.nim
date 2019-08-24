5ff0ed99c71e385bfe24faa2bf035a003fcf761d
Dmitry Ponyatov <dponyatov@gmail.com>
2019-08-24 21:59:43 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- habr.nim
+++ habr.nim
@@ -1,1 +1,8 @@
 # https://habr.com/ru/post/271197/
+
+echo "hello"
+
+# let name = stdin.readLine
+# echo "name=",name
+
+echo system.hostOS," @ ",system.hostCPU
---
+++ nim1.nim
@@ -0,0 +1,1 @@
+# https://howistart.org/posts/nim/1/
