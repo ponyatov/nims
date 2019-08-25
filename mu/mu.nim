05ebc254ec6e42b96a80fed0cd314b3c07344b41
Dmitry Ponyatov <dponyatov@gmail.com>
2019-08-25 18:07:18 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mu/mu.nim
+++ mu/mu.nim
@@ -1,1 +1,11 @@
 # match/update computing method
+
+const src = """12345
+abcdefgh"""
+
+echo src
+
+iterator chars(src:string): string = discard
+# for i in src: yield i
+
+for i in src.chars: echo i
