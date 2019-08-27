9314f4f114ec63b3cbabb60256366b60d9480600
Dmitry Ponyatov <dponyatov@gmail.com>
2019-08-27 17:33:15 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- nim1/brainfuck/src/gui.c
+++ nim1/brainfuck/src/gui.c
@@ -3,6 +3,17 @@
 
 #include <gtk/gtk.h>
 
+GtkWidget *window;
+
 extern void gui_init() {
-    gtk_init(&argc, &argv);
+    gtk_init(0,NULL);//&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_window_set_title(GTK_WINDOW(window), "Введение в GTK");
+    // gtk_container_set_border_width (GTK_CONTAINER(window), 50);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
 }
