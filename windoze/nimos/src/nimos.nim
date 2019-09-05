9573ac5f9cf0841460acddc5bb1d0ef6960d62ad
Dmitry Ponyatov <dponyatov@gmail.com>
2019-09-05 11:22:47 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- windoze/nimos/src/nimos.nim
+++ windoze/nimos/src/nimos.nim
@@ -3,3 +3,13 @@
 
 when isMainModule:
   echo("Hello, World!")
+
+# proc log(any: varargs[string,`$`]) = stdout.write any
+
+import os
+
+let exe  = getAppFileName()    ; echo "exe  ",exe
+let args = commandLineParams() ; echo "args ",args
+
+echo "Hello","world",1,3.4
+
