0a02c509c68aaa2b12e7b4dde10faeaef161fe1e
Dmitry Ponyatov <dponyatov@gmail.com>
2019-09-08 23:00:58 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- nimple/sample/hello.c
+++ nimple/sample/hello.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
+
+typedef struct {
+    int x;
+    float y;
+} STRUC;
+
+STRUC some;
+
+int sum(int a, float b) { return a+b; }
+
+STRUC* flo(int a, float b) { some.x = a; some.y = b; return &some; }
+
 int main() { printf("hello"); }
